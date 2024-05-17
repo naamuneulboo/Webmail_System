d7ce49539ae53c9b38a7015c8cb75af8c0d21c85
ggabaegii <117845415+ggabaegii@users.noreply.github.com>
2024-05-18 00:24:26 +0900

    메일 목록 인덱스 설정

--- src/main/java/deu/cse/spring_webmail/model/MessageFormatter.java
+++ src/main/java/deu/cse/spring_webmail/model/MessageFormatter.java
@@ -21,12 +21,20 @@ public class MessageFormatter {
     @NonNull private String userid;  // 파일 임시 저장 디렉토리 생성에 필요
     private HttpServletRequest request = null;
     
+    private int totalMessages;
+    private int endIndex;
+    
     // 220612 LJM - added to implement REPLY
     @Getter private String sender;
     @Getter private String subject;
     @Getter private String body;
-
-
+    
+    public MessageFormatter(String userid, int totalMessages, int endIndex) {
+        this.userid = userid;
+        this.totalMessages = totalMessages;
+        this.endIndex = endIndex;
+    }
+    
     public String getMessageTable(Message[] messages) {
         StringBuilder buffer = new StringBuilder();
 
@@ -43,18 +51,20 @@ public class MessageFormatter {
         for (int i = messages.length - 1; i >= 0; i--) {
             MessageParser parser = new MessageParser(messages[i], userid);
             parser.parse(false);  // envelope 정보만 필요
+            
+            int actualIndex = endIndex - i;
             // 메시지 헤더 포맷
             // 추출한 정보를 출력 포맷 사용하여 스트링으로 만들기
             buffer.append("<tr> "
-                    + " <td id=no>" + (i + 1) + " </td> "
+                    + " <td id=no>" + actualIndex + " </td> "
                     + " <td id=sender>" + parser.getFromAddress() + "</td>"
                     + " <td id=subject> "
-                    + " <a href=show_message?msgid=" + (i + 1) + " title=\"메일 보기\"> "
+                    + " <a href=show_message?msgid=" + actualIndex + " title=\"메일 보기\"> "
                     + parser.getSubject() + "</a> </td>"
                     + " <td id=date>" + parser.getSentDate() + "</td>"
                     + " <td id=delete>"
                     + "<a onclick=\"return confirm('정말로 삭제하시겠습니까?')\" href=delete_mail.do"
-                    + "?msgid=" + (i + 1) + "> 삭제 </a>" + "</td>"
+                    + "?msgid=" + actualIndex + "> 삭제 </a>" + "</td>"
                     + " </tr>");
         }
         buffer.append("</table>");
