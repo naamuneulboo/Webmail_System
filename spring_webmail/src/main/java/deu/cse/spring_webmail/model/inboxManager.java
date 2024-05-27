70e3ff18a1e39073689616e5915f3a2d6c6ba6e5
jshpro1 <128587810+jshpro1@users.noreply.github.com>
2024-05-27 21:37:39 +0900

    한글 되게끔

--- spring_webmail/src/main/java/deu/cse/spring_webmail/model/inboxManager.java
+++ spring_webmail/src/main/java/deu/cse/spring_webmail/model/inboxManager.java
@@ -4,21 +4,12 @@
  */
 package deu.cse.spring_webmail.model;
 
-import static com.mysql.cj.conf.PropertyKey.PASSWORD;
-import static jakarta.mail.Flags.Flag.USER;
-import static java.lang.System.out;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import static java.time.InstantSource.system;
 import java.util.ArrayList;
 import java.util.List;
-import lombok.Getter;
-import lombok.NonNull;
-import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
@@ -29,35 +20,6 @@ import java.util.regex.Matcher;
  */
 @Slf4j
 public class inboxManager {
-
-    @NonNull
-    @Getter
-    @Setter
-    private String userid;
-    @Getter
-    @Setter
-    private String toAddress;
-    @Getter
-    @Setter
-    private String fromAddress;
-    @Getter
-    @Setter
-    private String ccAddress;
-    @Getter
-    @Setter
-    private String sentDate;
-    @Getter
-    @Setter
-    private String subject;
-    @Getter
-    @Setter
-    private String body;
-    @Getter
-    @Setter
-    private String fileName;
-    @Getter
-    @Setter
-    private String downloadTempDir = "C:/temp/download/";
 
     private String mysqlServerIp;
     private String mysqlServerPort;
@@ -81,7 +43,7 @@ public class inboxManager {
 
     public List<inboxRow> getAllRows() {
         List<inboxRow> dataList = new ArrayList<>();
-        final String JDBC_URL = String.format("jdbc:mysql://%s:%s/webmail?serverTimezone=Asia/Seoul",
+        final String JDBC_URL = String.format("jdbc:mysql://%s:%s/webmail?useUnicode=true&characterEncoding=UTF-8&serverTimezone=Asia/Seoul",
                 mysqlServerIp, mysqlServerPort);
 
         log.debug("JDBC_URL: {}", JDBC_URL);
@@ -150,7 +112,7 @@ public class inboxManager {
     }
 
     public boolean deleteMessageById(String messageId) {
-        final String JDBC_URL = String.format("jdbc:mysql://%s:%s/webmail?serverTimezone=Asia/Seoul",
+        final String JDBC_URL = String.format("jdbc:mysql://%s:%s/webmail?useUnicode=true&characterEncoding=UTF-8&serverTimezone=Asia/Seoul",
                 mysqlServerIp, mysqlServerPort);
 
         log.debug("JDBC_URL: {}", JDBC_URL);
@@ -187,7 +149,7 @@ public class inboxManager {
     }
 
     public String getMessageById(String messageId) {
-        final String JDBC_URL = String.format("jdbc:mysql://%s:%s/webmail?serverTimezone=Asia/Seoul",
+        final String JDBC_URL = String.format("jdbc:mysql://%s:%s/webmail?useUnicode=true&characterEncoding=UTF-8&serverTimezone=Asia/Seoul",
                 mysqlServerIp, mysqlServerPort);
 
         log.debug("JDBC_URL: {}", JDBC_URL);
